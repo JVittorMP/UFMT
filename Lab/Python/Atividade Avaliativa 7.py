eb724348885f5b8b48e5f027f5046ce6a3ed2aba
João Vittor de Moura Padilha <103512187+JVittorMP@users.noreply.github.com>
2022-07-26 00:03:28 -0400

    Python Branch

---
+++ Lab/Python/Atividade Avaliativa 7.py
@@ -0,0 +1,34 @@
+CPF = []
+soma = 0
+print("insira o CPF: ", end = "")
+str_CPF = input()
+
+for i in str_CPF: # Transforma a Entrada em uma Lista
+    CPF.append(i)
+
+for i in range(9): # Realiza a Soma
+    aux = int(CPF[i])*(10-i)
+    soma = soma + aux
+
+dig1 = soma%11 # Encontra o 1º Dígito
+if(dig1 >= 2):
+    dig1 = 11 - dig1
+else:
+    dig1 = 0
+
+soma = 0
+for i in range(10): # Realiza a Nova Soma
+    aux = int(CPF[i])*(11-i)
+    soma = soma + aux
+
+dig2 = soma%11 # Encontra o 2º Dígito
+if(dig2 >= 2):
+    dig2 = 11 - dig2
+else:
+    dig2 = 0
+
+if(int(CPF[9]) == dig1 and int(CPF[10]) == dig2): # Verifica a Validade
+    print("CPF Válido!")
+else:
+    print("CPF Inválido!")
+    print(f"Dígitos Verificadores Corretos: {dig1}{dig2}")
